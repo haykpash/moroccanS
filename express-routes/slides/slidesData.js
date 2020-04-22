407f728139ab73c74193ed3b4f47a6c543cf2ee7
Hayk Pashinyan <24hayk24@gmail.com>
2020-04-21 19:40:28 -0700

    2456

--- express-routes/slides/slidesData.js
+++ express-routes/slides/slidesData.js
@@ -2,13 +2,16 @@ module.exports = [
     {
         arrKey: 11,
         image: '/img/Slideshow/slide-1.jpg',
+        title: 'image1',
     },
     {
         arrKey: 12,
         image: '/img/Slideshow/slide-2.jpg',
+        title: 'image2',
     },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
+        title: 'image3',
     },
 ]
--- src/App/Pages/Home/Slideshow/Slideshow.jsx
+++ src/App/Pages/Home/Slideshow/Slideshow.jsx
@@ -10,14 +10,17 @@ const defaultSlides = [
     {
         arrKey: 11,
         image: '/img/Slideshow/slide-1.jpg',
+        title: 'image1',
     },
     {
         arrKey: 12,
         image: '/img/Slideshow/slide-2.jpg',
+        title: 'image2',
     },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
+        title: 'image3',
     },
 ];
 
