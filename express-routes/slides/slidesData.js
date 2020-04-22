71992531da0b8ac7a8089d5f9b0922983def3315
Hayk Pashinyan <24hayk24@gmail.com>
2020-04-21 19:33:03 -0700

    third slide

--- express-routes/slides/slidesData.js
+++ express-routes/slides/slidesData.js
@@ -3,10 +3,10 @@ module.exports = [
         arrKey: 11,
         image: '/img/Slideshow/slide-1.jpg',
     },
-    // {
-    //     arrKey: 12,
-    //     image: '/img/Slideshow/slide-2.jpg',
-    // },
+    {
+        arrKey: 12,
+        image: '/img/Slideshow/slide-2.jpg',
+    },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
--- src/App/Pages/Home/Slideshow/Slideshow.jsx
+++ src/App/Pages/Home/Slideshow/Slideshow.jsx
@@ -11,10 +11,10 @@ const defaultSlides = [
         arrKey: 11,
         image: '/img/Slideshow/slide-1.jpg',
     },
-    // {
-    //     arrKey: 12,
-    //     image: '/img/Slideshow/slide-2.jpg',
-    // },
+    {
+        arrKey: 12,
+        image: '/img/Slideshow/slide-2.jpg',
+    },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
