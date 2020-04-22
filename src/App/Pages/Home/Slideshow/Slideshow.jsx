48e47feb41ec601f52665e823d27ef45e6a28b01
Hayk Pashinyan <24hayk24@gmail.com>
2020-04-21 20:15:20 -0700

    789

--- src/App/Pages/Home/Slideshow/Slideshow.jsx
+++ src/App/Pages/Home/Slideshow/Slideshow.jsx
@@ -12,11 +12,11 @@ const defaultSlides = [
         image: '/img/Slideshow/slide-1.jpg',
         title: 'image1',
     },
-    // {
-    //     arrKey: 12,
-    //     image: '/img/Slideshow/slide-2.jpg',
-    //     title: 'image2',
-    // },
+    {
+        arrKey: 12,
+        image: '/img/Slideshow/slide-2.jpg',
+        title: 'image2',
+    },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
