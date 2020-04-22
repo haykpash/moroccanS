07710f7021ac4023a439dbeedf64a64ed91754e3
Hayk Pashinyan <24hayk24@gmail.com>
2020-04-21 19:50:05 -0700

    hhh

--- express-routes/slides/slidesData.js
+++ express-routes/slides/slidesData.js
@@ -4,11 +4,11 @@ module.exports = [
         image: '/img/Slideshow/slide-1.jpg',
         title: 'image1',
     },
-    {
-        arrKey: 12,
-        image: '/img/Slideshow/slide-2.jpg',
-        title: 'image2',
-    },
+    // {
+    //     arrKey: 12,
+    //     image: '/img/Slideshow/slide-2.jpg',
+    //     title: 'image2',
+    // },
     {
         arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
