a11d8a99f2040f3d3a6bf22a3c2c46c1c43af48b
Hayk Pashinyan <24hayk24@gmail.com>
2020-04-21 19:28:15 -0700

    heroku

--- express-routes/slides/slidesData.js
+++ express-routes/slides/slidesData.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        arrKey: 0,
+        arrKey: 11,
         image: '/img/Slideshow/slide-1.jpg',
     },
+    // {
+    //     arrKey: 12,
+    //     image: '/img/Slideshow/slide-2.jpg',
+    // },
     {
-        arrKey: 1,
-        image: '/img/Slideshow/slide-2.jpg',
-    },
-    {
-        arrKey: 2,
+        arrKey: 13,
         image: '/img/Slideshow/slide-3.jpg',
     },
 ]
