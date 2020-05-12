606635de25c8887b0dcea10f1b6a429ec4e10097
Hayk Pashinyan <24hayk24@gmail.com>
2020-05-12 12:33:09 -0700

    Adding shadow

--- src/App/Header/Header.jsx
+++ src/App/Header/Header.jsx
@@ -3,14 +3,16 @@ import './Header.scss';
 
 import Title from './Title/Title.jsx';
 import Bag from './Bag/Bag.jsx';
+import Logo from './Logo/Logo';
 
 const Header = () => {
     return (
     <div className="header">
         <div className="comp">
+            <Logo />
             <Title />
-            <Bag />
         </div>
+            <Bag />
     </div>
     )
 };
---
+++ src/App/Header/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Logo.scss';
+
+
+
+const Logo  = () => {
+   return (
+      <div className="Logo">
+         <img src='/img/Logo/Logo_img.png' alt='logo'/>
+      </div>
+   )
+};
+
+export default Logo;
