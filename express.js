b0c10579ec9a18f2ff45202395a37b4f4d26718b
Hayk Pashinyan <24hayk24@gmail.com>
2020-09-21 20:37:53 -0700

    image resoutios change

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 // Initiaize Instance of Express as app
 const app = express();
-
+app.disable('x-powered-by')  // for safety 
 // POST Requests: In order to accept post requests, you must use bodyParser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
