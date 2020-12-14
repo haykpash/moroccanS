70b8ff84c32aa6d957293e80c220acbbac0e5f03
haykpash <hpashinyan@gmail.com>
2020-12-13 18:32:22 -0800

    avatar

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -10,3 +10,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
     })
   );
 };
+
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+//   module.exports = function(app) {
+
+//   app.use(
+//     '/api',
+//       createProxyMiddleware({
+//       target: 'http://localhost:5000/',
+//       changeOrigin: true
+//     })
+//   );
+// };
